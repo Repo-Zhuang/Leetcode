d0f40e60d7385ab777e372f4272db850f369bd67
Coup De Grace <82269483@qq.com>
2022-09-30 15:22:55 +0800

    225

---
+++ 225.用队列实现栈.cpp
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=225 lang=cpp
+ *
+ * [225] 用队列实现栈
+ */
+
+// @lc code=start
+class MyStack {
+public:
+    queue<int> que1;
+    queue<int> que2; 
+
+    MyStack() {
+
+    }
+    
+    void push(int x) {
+        que1.push(x);
+    }
+    
+    int pop() {
+        int size = que1.size();
+        size--;
+        while (size--) { // 将que1 导入que2，但要留下最后一个元素
+            que2.push(que1.front());
+            que1.pop();
+        }
+
+        int result = que1.front(); // 留下的最后一个元素就是要返回的值
+        que1.pop();
+        que1 = que2;            // 再将que2赋值给que1
+        while (!que2.empty()) { // 清空que2
+            que2.pop();
+        }
+        return result;
+    }
+    
+    int top() {
+        return que1.back();
+    }
+    
+    bool empty() {
+        return que1.empty();
+    }
+};
+
+/*
+//用一个队列的方法
+class MyStack {
+public:
+    queue<int> que;
+
+    MyStack() {
+
+    }
+    
+    void push(int x) {
+        que.push(x);
+    }
+    
+    int pop() {
+        int size = que.size();
+        size--;
+        while (size--) { // 将最后一个元素前面的元素都添加都前面
+            que.push(que.front());
+            que.pop();
+        }
+
+        int result = que.front(); // 留下的最后一个元素就是要返回的值
+        que.pop();
+        return result;
+    }
+    
+    int top() {
+        return que.back();
+    }
+    
+    bool empty() {
+        return que.empty();
+    }
+};
+*/
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+// @lc code=end
+
