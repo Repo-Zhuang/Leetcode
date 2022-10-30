4e265b7307fdcb379550b40bc3364a3097592f4b
Coup De Grace <82269483@qq.com>
2022-10-30 11:32:15 +0800

    784

---
+++ 784.字母大小写全排列.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=784 lang=cpp
+ *
+ * [784] 字母大小写全排列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void dfs(string &s, int pos, vector<string> &res) {
+        while (pos < s.size() && isdigit(s[pos])) {
+            pos++;
+        }
+        if (pos == s.size()) {
+            res.emplace_back(s);
+            return;
+        }
+        s[pos] ^= 0x20;
+        dfs(s, pos + 1, res);
+        s[pos] ^= 0x20;
+        dfs(s, pos + 1, res);
+    }
+
+    vector<string> letterCasePermutation(string s) {
+        vector<string> ans;
+        dfs(s, 0, ans);
+        return ans;
+    }
+};
+// @lc code=end
+
